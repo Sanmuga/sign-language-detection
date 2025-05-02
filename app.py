659473171fa0c6e172aa606c201dd37e473a3cea
SANMUGAVADIVEL K <93981311+Sanmuga@users.noreply.github.com>
2025-05-02 16:07:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,8 @@ def video_feed():
 def get_text():
     return jsonify({"text": text_output})
 
-    
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  
-    app.run(host='0.0.0.0', port=port, debug=True) 
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
+
